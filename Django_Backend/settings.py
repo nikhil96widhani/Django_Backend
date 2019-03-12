b2825f9b5b9bcfd0550cad2ea295932b0eaa6ff2
Nikhil <nikhil96widhani@gmail.com>
2019-03-12 11:02:28 -0400

    First Commit

--- Django_Backend/settings.py
+++ Django_Backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'u15.apps.U15Config',
+    'institutions.apps.InstitutionsConfig'
 ]
 
 MIDDLEWARE = [
--- Django_Backend/urls.py
+++ Django_Backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from institutions import views as institutions_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', institutions_views.register, name='register'),
     path('', include('u15.urls'))
 ]
